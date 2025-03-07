42715b314a622e765e0188aac3939234108d3716
leandro <leandrolopez@kamk.fi>
2025-03-07 02:10:26 +0200

    exercises 4-6 completed

--- M05_threads/src/my_code.py
+++ M05_threads/src/my_code.py
@@ -1,3 +1,4 @@
+"""
 import threading
 import time
 
@@ -18,5 +19,37 @@ def wait_threads(th_list):
 #Test software under this if
 if __name__ == "__main__":
     th_list=start_threads(heavy_computing, N)
+    wait_threads(th_list)
+    """
+import threading
+import time
+
+# heavy_computing for test purposes!
+# You may modify the function if necessary
+if __name__ == "__main__":
+    def heavy_computing(idx):
+        print('->heavy_computing('+str(idx)+')')
+        time.sleep(10)
+        print('<-heavy_computing('+str(idx)+')')
+
+def start_threads(f, N):
+    """Inicia N hilos ejecutando la función f con índices de 0 a N-1"""
+    th_list = []
+    # Crear y empezar cada hilo
+    for i in range(N):
+        t = threading.Thread(target=f, args=(i,))
+        th_list.append(t)
+        t.start()  # Iniciar el hilo inmediatamente
+    return th_list
+
+def wait_threads(th_list):
+    """Espera a que todos los hilos en th_list terminen"""
+    for t in th_list:
+        t.join()  # Esperar a que cada hilo complete
+
+# Test software under this if
+if __name__ == "__main__":
+    N = 10
+    th_list = start_threads(heavy_computing, N)
     wait_threads(th_list)
 
--- M06_threads/src/my_code.py
+++ M06_threads/src/my_code.py
@@ -1,3 +1,4 @@
+""""
 import threading
 import concurrent.futures as cf
 import time
@@ -25,5 +26,45 @@ if __name__ == "__main__":
     th_list=start_threads(heavy_computing, N)
     print('Wait...')
     ret=wait_threads(th_list)
+    print('All futures completed')
+    print(ret)
+"""
+import threading
+import concurrent.futures as cf
+import time
+
+# heavy_computing for test purposes!
+# You may modify the function if necessary
+if __name__ == "__main__":
+    def heavy_computing(idx):
+        print('->heavy_computing('+str(idx)+')')
+        time.sleep(10)
+        print('<-heavy_computing('+str(idx)+')')
+        return idx, idx*idx
+
+def start_threads(f, N):
+    """Inicia N tareas concurrentes ejecutando f con índices de 0 a N-1"""
+    executor = cf.ThreadPoolExecutor(max_workers=N)
+    futures = [executor.submit(f, i) for i in range(N)]
+   
+    return futures
+
+def wait_threads(th_list):
+    """Espera a que todas las tareas en th_list terminen y devuelve los resultados"""
+    
+    results = [future.result() for future in cf.as_completed(th_list)]
+    
+    sorted_results = sorted(results, key=lambda x: x[0])
+   
+    return [r[1] for r in sorted_results]
+
+# Test software under this if        
+if __name__ == "__main__":
+    N = 10
+
+    print('None started')
+    th_list = start_threads(heavy_computing, N)
+    print('Wait...')
+    ret = wait_threads(th_list)
     print('All futures completed')
     print(ret)
